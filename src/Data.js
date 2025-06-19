bd50c95d11c85b00636e056b08ad1ed5e7ef1c11
Jatin2662 <creativejatin111@gmail.com>
2025-06-19 23:27:37 +0530

    Placed all static data in separate file for readibility

---
+++ src/Data.js
@@ -0,0 +1,164 @@
+
+
+
+const experience = [
+    {
+        id: 1,
+        title: 'Apprentice at BSNL (Oct 2024 - Present)',
+        description: [
+            'Assisted in the CAF Warehouse and performed quality checks on data.',
+            'Developed communication and problem-solving skills through customer interactions and coordination with seniors.'
+        ]
+    },
+    {
+        id: 2,
+        title: 'Internship, Trainee (Jan 2024 - June 2024)',
+        description: [
+            'Developed full-stack web applications using the MERN (MongoDB, Express, React, Node.js) stack.',
+            'Collaborated with team members to design user-friendly interfaces and implement backend functionalities.'
+        ]
+    }
+]
+
+const projects = [
+    {
+        id: 1,
+        title: 'Habit Goal Tracker',
+        liveLink: 'https://habitgoaltracker-1.onrender.com',
+        timePeriod: 'April 2025 - May 2025',
+        technology: [
+            'Built a website to manage habits using MERN Stack.',
+            'Managed both frontend and backend, integrated authentication, and implemented role-based access and toasts.'
+        ]
+    },
+    {
+        id: 2,
+        title: 'Home Groom Website',
+        timePeriod: 'March 2024 - May 2024',
+        technology: [
+            'Created a platform for hiring household helpers using MERN Stack.',
+            'Managed both frontend and backend, ensuring a seamless user experience.',
+            'Implemented responsive design, increasing user engagement.'
+        ]
+    },
+    {
+        id: 3,
+        title: 'Gesture Control',
+        timePeriod: 'October 2023',
+        technology: [
+            'Developed a system to control devices using hand gestures with Python and OpenCV.',
+            'Led the team and designed an intuitive UI, enhancing user interaction.'
+        ]
+    },
+    {
+        id: 4,
+        title: 'Exhibition App',
+        timePeriod: 'May 2023',
+        technology: [
+            'Built an app to showcase exhibition products using Flutter, Dart, and Firebase.',
+            'Handled UI/UX design and backend management, resulting in improved navigation.'
+        ]
+    },
+]
+
+const skills = [
+    {
+        type: 'Frontend',
+        stack: [
+            {
+                id: 1,
+                title: "HTML",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+                alt: 'html',
+                position: 'left'
+            },
+            {
+                id: 2,
+                title: "CSS",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+                alt: 'css',
+                position: 'top'
+            },
+            {
+                id: 3,
+                title: "JavaScript",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+                alt: 'javascript',
+                position: 'bottom'
+            },
+            {
+                id: 4,
+                title: "React",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+                alt: 'react',
+                position: 'right'
+            },
+        ]
+    },
+    {
+        type: 'Backend',
+        stack: [
+            {
+                id: 1,
+                title: "Node.js",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+                alt: 'node.js',
+                position: 'left'
+            },
+            {
+                id: 2,
+                title: "Express.js",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
+                alt: 'express.js',
+                position: 'top'
+            },
+            {
+                id: 3,
+                title: "MongoDB",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
+                alt: 'monogDB',
+                position: 'bottom'
+            },
+            {
+                id: 4,
+                title: "Mongoose",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg",
+                alt: 'mongoose',
+                position: 'right'
+
+            }
+
+        ]
+    },
+    {
+        type: 'Tools',
+        stack: [
+            {
+                id: 1,
+                title: "Postman",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
+                alt: 'postman',
+                position: 'left'
+            },
+            {
+                id: 2,
+                title: "Git",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+                alt: 'git',
+                position: 'bottom'
+            },
+            {
+                id: 3,
+                title: "VS Code",
+                link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg",
+                alt: 'vs code',
+                position: 'right'
+            },
+        ]
+    }
+]
+
+
+export {
+    experience, projects, skills
+}
